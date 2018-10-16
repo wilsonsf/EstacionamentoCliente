857155ea89fa11f191dff0c8a7efc222ebe55266
Wilson Farias <wilsonsf1@gmail.com>
2018-10-16 10:08:59 -0300

    Corrige exibição de vagas no módulo Vaga

--- js/vaga.js
+++ js/vaga.js
@@ -45,73 +45,12 @@ function excluirVaga() {
     });
 }
 
-
-function updateVagasLivres() {
-    let vagas = getVagasLivres();
-
-    let vagasLivres = document.getElementById('vagas-livres')
-    vagasLivres.innerHTML = '';
-    vagas.forEach(function (element, index, array) {
-        vagasLivres.innerHTML +=
-            "<li class='list-group-item' id='" + element._id + "'>" + element.toString() + "</li>"
-
-    });
-}
-
-function getVagasLivres() {
-    let vagas = [];
-    $.getJSON("http://localhost:3000/vaga/desocupada", function (data) {
-        $.each(data, function (key, val) {
-            let id = val['_id'];
-            let andar = val['andar'];
-            let numero = val['numero'];
-            let ocupada = val['ocupada'];
-
-            let v = new Vaga(id, andar, numero, ocupada);
-
-            vagas.push(v);
-        });
-    });
-
-    return vagas;
-}
-
-function updateVagasOcupadas() {
-    let vagas = getVagasOcupadas();
-
-    let vagasOcupadas = document.getElementById('vagas-ocupadas');
-    vagasOcupadas.innerHTML = '';
-    vagas.forEach(function (element, index, array) {
-        vagasOcupadas.innerHTML +=
-            "<li class='list-group-item' id='" + element._id + "'>" + element.toString() + "</li>"
-    });
-}
-
-function getVagasOcupadas() {
-    let vagas = [];
-
-    $.getJSON("http://localhost:3000/vaga/ocupada", function (data) {
-        $.each(data, function (key, val) {
-            let id = val['_id'];
-            let andar = val['andar'];
-            let numero = val['numero'];
-            let ocupada = val['ocupada'];
-
-            let v = new Vaga(id, andar, numero, ocupada);
-
-            vagas.push(v);
-        });
-    });
-
-    return vagas;
-}
-
 function atualizaListagens() {
-    updateVagasLivres();
-    updateVagasOcupadas();
+    getVagasLivres();
+    getVagasOcupadas();
 }
 
 $(document).ready(function () {
-    updateVagasLivres();
+    atualizaListagens();
 });
 
